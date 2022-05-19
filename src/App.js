8da0a47c5910ba54e8a86f3822ed3b20379cbbee
Lautaro Conte <lauty.conte@gmail.com>
2022-05-18 22:26:21 -0300

    :tada: Commit v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Counter from './Components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      finished: false,
+      winner: ''
+    }
+  }
+
+  handleWin (winner) {
+    this.setState({ finished: true, winner });
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <div className="title">
+          <h1> Anotador de Truco </h1>
+        </div>
+        <div className="board">
+          { this.state.finished ?
+            <div className="finished-message">
+              <h3>{ `Ganador: ${ this.state.winner }` }</h3>
+              <button
+                className="restart-button"
+                onClick={ () => { this.setState({ finished: false }) } }
+              >
+                Reiniciar
+              </button>
+            </div>
+            :
+            <>
+              <Counter
+                title="Nosotros"
+                onWin={ () => { this.handleWin('Nosotros') } }
+              />
+
+              <Counter
+                title="Ellos"
+                onWin={ () => { this.handleWin('Ellos') } }
+              />
+            </>
+          }
+        </div>
+        <div className="footer">
+        <span className="footer__copy">
+                    &#169;Copyright, <script>document.write(new Date().getFullYear())</script>
+                     <span className="footer__title"><b><a href='https://github.com/lauticonte' target='_blank' rel='noreferrer'>Conte</a></b></span>. All rights reserved.
+                </span>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Counter.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import './Counter.css';
+
+class Counter extends React.Component {
+    
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            points: 0
+        }
+
+        this.stage      = 'Malas';
+        this.lineLength = 90;
+        this.offsetX    = 5;
+        this.offsetY    = 7;
+        this.box        = 0;
+
+        this.mask = [
+            { 
+                x1: 0, y1: 0, 
+                x2: this.lineLength, y2: 0 
+            },
+            { 
+                x1: 0, y1: 0, 
+                x2: 0, y2: this.lineLength 
+            },
+            { 
+                x1: 0, y1: this.lineLength, 
+                x2: this.lineLength, y2: this.lineLength 
+            },
+            { 
+                x1: this.lineLength, y1: 0, 
+                x2: this.lineLength, y2: this.lineLength 
+            },
+            { 
+                x1: 0, y1: 0, 
+                x2: this.lineLength, y2: this.lineLength }
+        ];
+
+        this.renderLine     = this.renderLine.bind(this);
+        this.renderLines    = this.renderLines.bind(this);
+        this.addPoint       = this.addPoint.bind(this);
+        this.subtractPoint  = this.subtractPoint.bind(this);
+    }
+
+    addPoint () {
+        this.setState(state => {
+            if (this.stage === 'Buenas' && state.points === 15) return null;
+
+            let nextPoints = state.points + 1;
+
+            if(nextPoints === 16) {
+                this.stage = 'Buenas';
+                nextPoints = 1;
+            }   
+
+            if (nextPoints === 15 && this.stage === 'Buenas') setTimeout(this.props.onWin, 200);
+
+            return { points: nextPoints }
+        });
+    }
+
+    subtractPoint () {
+        this.setState(state => {
+            if (this.stage === 'Malas' && state.points === 0) return null;
+
+            let nextPoints = state.points - 1;
+
+            if (nextPoints === 0 && this.stage === 'Buenas') {
+                this.stage = 'Malas';
+                nextPoints = 15;
+            }
+
+            return { points: nextPoints }
+        });
+    }
+
+    renderLines () {
+        const lines = [];
+        let box = 0;
+        for (let i = 0; i < this.state.points; i++) {
+            if (i / 5 === ( 1 + box )) ++box;
+            lines.push(this.renderLine(this.mask[i % 5], box, i));
+        }
+
+        return lines;
+    }
+
+    renderLine ({ x1, y1, x2, y2 }, box, key) {
+        return <line 
+        x1={ x1 + this.offsetX } 
+        y1={ y1 + this.offsetY + (box * (this.offsetY + this.lineLength)) } 
+        
+        x2={ x2 + this.offsetX } 
+        y2={ y2 + this.offsetY + (box * (this.offsetY + this.lineLength)) } 
+        
+        key={ key }
+        stroke="#ECDBBA" 
+        strokeWidth="5"
+        strokeLinecap="round"
+        />
+    }
+
+
+    render () {
+        const stageIndicatorColorStyle = { background: this.stage === 'Buenas' ? '#4287f5' : '#C84B31', color: '#ECDBBA' };
+
+        const titleStyle = { className: this.props.title === 'Nosotros' ? 'counter-body-nos' : 'counter-body-ellos' };
+
+        const string = titleStyle.className.toString();
+
+        return (
+            <div className={string}>
+                <div className="counter-title">
+                <h2>{ this.props.title }</h2>
+                </div>
+                <div className="stage-indicator">
+                <h3 style={ stageIndicatorColorStyle }>{ this.stage }</h3>
+                </div>
+                <svg
+                className="svg-canvas"
+                viewBox="0 0 100 300"
+                width="100px" 
+                height="300px"
+                >
+                    { this.renderLines() }
+                </svg>
+                <div
+                className="counter-buttons-container">
+                    <button 
+                    className="counter-button"
+                    onClick={ this.addPoint }
+                    > + </button>
+                    <button 
+                    className="counter-button"
+                    onClick={ this.subtractPoint }
+                    > - </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Counter;
