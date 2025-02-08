43d0af637e0e1333f6dbbd73f8c65632b49d6865
Lautaro Conte <info@contelautaro.com.ar>
2025-02-07 23:30:12 -0300

    🐛 bug cafecito

--- src/components/Header.js
+++ src/components/Header.js
@@ -115,6 +115,10 @@ const Header = React.memo(
               </div>
             </li>
           </ul>
+          <div className="sidebar-footer">
+            <span>¡Ayudanos a mantener la app!</span>
+            <a href='https://cafecito.app/truqito' rel='noreferrer' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_5.png 1x, https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_5.png' alt='Invitame un café en cafecito.app' /></a>
+          </ div>      
         </div>
 
         {/* PERFIL DEL USUARIO */}
