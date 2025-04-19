373d76dd1ba55b191f75740a080f529e05e5e241
Lautaro Conte <lauty.conte@gmail.com>
2025-04-19 00:18:09 -0300

    ⚡️ hide intersitial

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ const App = () => {
           </Routes>
         </main>
         <Footer />
-        <AdComponent adId="127560-15" type="15" siteId="127560" formatId="15" />
+        {/* Oculto Intersitial <AdComponent adId="127560-15" type="15" siteId="127560" formatId="15" /> */}
         <AdComponent adId="127560-6" type="6" siteId="127560" formatId="6" />
         <History isVisible={isHistoryVisible} onClose={toggleHistory} teamNames={teamNames} />
         <EditNames
